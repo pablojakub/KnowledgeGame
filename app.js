c528826af09aabd95238e116616ce8b3ac0d006f
jakubowskip <pawel.jakubowski92@gmail.com>
2025-04-09 13:44:43 +0200

    .

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
+    console.log('Działam');
     res.sendFile('index.html');
 });
 
